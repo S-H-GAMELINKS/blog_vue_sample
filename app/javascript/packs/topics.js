6f9a7cd8191899ba682cbb6435f8feb801595321
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-17 16:12:39 +0900

    fix topics.js

--- app/javascript/packs/topics.js
+++ app/javascript/packs/topics.js
@@ -1,13 +1,9 @@
 import Vue from 'vue/dist/vue.esm'
 import Index from './components/topics/index.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: '#topics',
-    components: {
-      'topic-index': Index,
-    }
-  })
-
-  console.log(app)
+const app = new Vue({
+  el: '#topics',
+  components: {
+    'topic-index': Index,
+  }
 })
